f06c9248ddec34b865dfad46b5ef94758dfb2f07
Mihai <dimamihaimail@gmail.com>
2023-10-21 14:20:31 +0300

    first commit

---
+++ src/main/java/com/eventflow/eventflowout/EventflowoutApplication.java
@@ -0,0 +1,13 @@
+package com.eventflow.eventflowout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventflowoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventflowoutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eventflow/eventflowout/EventflowoutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventflow.eventflowout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventflowoutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
